5d9705732a3aec284c5eebdb0289dd9c47f372bc
oliveraw <oliveraw@umich.edu>
2024-04-21 17:07:41 -0400

    shell script to run 100 test examples (human_goals=False)

---
+++ Rememberer/launchw_test.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# Copyright 2023 SJTU X-Lance Lab
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Created by Danyang Zhang @X-Lance.
+
+date_str=$(date +%Y-%m-%dT%H:%M:%S)
+
+python webshop.py --log-dir logs\
+				  --observation-mode text_rich\
+				  --load-replay history-pools/init_pool.wq.yaml\
+				  --load-replay history-pools/init_pool.wq.yaml\
+				  --save-replay history-pools/init_pool.wqu."$date_str".%d.a.yaml\
+				  --save-replay history-pools/init_pool.wqu."$date_str".%d.b.yaml\
+				  --item-capacity 500\
+				  --action-capacity 20\
+				  --matcher pgpat+insrel\
+				  --prompt-template prompts/\
+				  --max-tokens 200\
+				  --stop "Discouraged"\
+				  --request-timeout 10.\
+				  --starts-from 0\
+				  --epochs 1\
+				  --trainseta 0\
+				  --trainsetb 100\
+				  --testseta 0\
+				  --testsetb 10
