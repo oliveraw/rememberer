aae3f9065dafa8943966a9d5ff8661119f184873
NikashPrakash <86813719+NikashPrakash@users.noreply.github.com>
2024-04-28 23:39:17 -0400

    LLama model

--- Rememberer/agent_protos.py
+++ Rememberer/agent_protos.py
@@ -217,6 +217,113 @@ class OpenAIClient(abc.ABC, Generic[A]):
     def _parse_action(self, response: str) -> A:
         raise NotImplementedError()
 
+class LlamaModel(abc.ABC, Generic[A]):
+    def __init__( self
+                , prompt_templates: TemplateGroup
+                , ckpt_dir: str = "../Meta-Llama-3-8B/"
+                , max_tokens: int = 20
+                , temperature: float = 0.1
+                , top_p: float = 0.9
+                , stop: Optional[str] = None
+                , request_timeout: float = 5.
+                , request_pause: float = 3.1
+                , manual: bool = False
+                ):
+        """
+        Args:
+            prompt_templates (TemplateGroup): templates for the prompt
+
+            api_key (str): openai api key
+            model (str): the model to use
+
+            max_tokens (int): max number of tokens to generate
+            temperature (float): generating temperature
+            stop (Optional[str]): stop sequence for the model
+
+            request_timeout (float): waiting time for the client to timeout
+            request_pause (float): waiting time between two consecutive request
+            manual (bool):
+        """
+
+        self._prompt_templates: TemplateGroup = prompt_templates
+
+
+        self.max_tokens: int = max_tokens
+        self.temperature: float = temperature
+        self._stop: Optional[str] = stop
+
+        self._request_timeout: float = request_timeout
+        self._request_pause: float = request_pause
+        self._manual: bool = manual
+
+        self._last_request_time: datetime.datetime = datetime.datetime.now()
+
+        self.top_p = top_p
+        # print(torch.cuda.memory_summary(device=None, abbreviated=False))
+        self.model = Llama.build(ckpt_dir, ckpt_dir+"tokenizer.model", 1024,4, 1)
+    
+    def _get_response(self, prompt: List[Dialog]) -> Optional[A]:
+        """
+        Args:
+            prompt (str): the input prompt
+
+        Returns:
+            Optional[A]: the completion text
+        """
+
+        try:
+            if not self._manual:
+                request_time = datetime.datetime.now()
+                timedelta: datetime.timedelta = request_time - self._last_request_time
+                timedelta: float = timedelta.total_seconds()
+                if self._request_pause - timedelta > 0.:
+                    time.sleep(self._request_pause-timedelta)
+
+                              
+                # output = self.model.generate(
+                #     input_ids,
+                #     max_gen_len=input_ids.shape[1] + self.max_tokens,
+                #     temperature=self.temperature,
+                #     top_p=self.top_p
+                # )
+                response = self.model.chat_completion(prompt, 
+                                                      self.temperature,
+                                                      self.top_p, 
+                                                      self.max_tokens
+                                                      )[0]['generation']['content']
+                
+                self._last_request_time = datetime.datetime.now()
+                print(response)
+                logger.info( f"Return: {response}")
+            else:
+                prompt: str
+                single_line_response: str = input(prompt)
+                response: List[str] = []
+                while single_line_response!="":
+                    response.append(single_line_response)
+                    single_line_response = input()
+                response: str = "\n".join(response)
+
+                logger.debug( "Response: %s"
+                            , response
+                            )
+
+            action: A = self._parse_action(response)
+        except Exception as e:
+            with io.StringIO() as bfr:
+                ocounter = globals()["ocounter"]
+                traceback.print_exc(file=bfr)
+                ologger.debug("%d: %s", ocounter, bfr.getvalue())
+                logger.debug("Response error %d, %s", ocounter, str(type(e)))
+                globals()["ocounter"] += 1
+            action = None
+
+        return action
+
+    @abc.abstractmethod
+    def _parse_action(self, response: str) -> A:
+        raise NotImplementedError()
+
 class HistoryReplayClient(Generic[history.Key, history.Action]):
     #  class HistoryReplayClient {{{ # 
     def __init__( self
@@ -398,6 +505,165 @@ class HistoryReplayClient(Generic[history.Key, history.Action]):
     def train(self, train: bool):
         self._train = train
     #  }}} class HistoryReplayClient # 
+    def __init__( self
+                , history_replay: history.HistoryReplay[history.Key, history.Action]
+                , train: bool
+                , tokenizer: Tokenizer
+                , norandom: bool = False
+                ):
+        #  method __init__ {{{ # 
+        self._history_replay: history.HistoryReplay[history.Key, history.Action]\
+                = history_replay
+        self._train: bool = train
+
+        self._rng: np.random.Generator = np.random.default_rng()
+        self._tokenizer = tokenizer
+
+        self._norandom: bool = norandom
+        #  }}} method __init__ # 
+
+    def _get_examplars( self
+                      , key: history.Key
+                      , example_tokens_limit: int
+                      , nb_examplars: int = 2
+                      ) -> List[str]:
+        #  method _get_examplars {{{ # 
+        """
+        Args:
+            key (history.Key): the key to retrieve
+            example_tokens_limit (int): length limit for the examplar strs
+            nb_examplars (int): the number of examplars to retrieve
+
+        Returns:
+            List[str]: examplar strs
+        """
+
+        candidates: List[ Tuple[ history.Key
+                               , history.HistoryReplay.Record[history.Action]
+                               , float
+                               ]
+                        ] = self._history_replay[key]
+        # print("Candidates: ", candidates)
+        #  Construct Examplars {{{ # 
+        examplars: List[str] = []
+        examplar_ids: List[int] = []
+        examplar_scores: List[float] = []
+        #nb_examplars = 2
+        i = 0
+        for cdd in candidates:
+            #  Contruct one Examplar {{{ # 
+            key: history.Key
+            record: history.HistoryReplay.Record[history.Action]
+            score: float
+            key, record, score = cdd
+            info_dict: history.HistoryReplay.InfoDict[history.Action] = record["other_info"]
+
+            action_dict: history.HistoryReplay.ActionDict[history.Action] = record["action_dict"]
+            actions: List[Tuple[history.Action, float]] =\
+                    sorted( map( lambda itm: (itm[0], itm[1]["qvalue"])
+                               , action_dict.items()
+                               )
+                          , key=(lambda itm: itm[1])
+                          , reverse=True
+                          )
+            # print(actions)
+            logger.info(actions)
+            if actions[0][1]<=0.:
+                if self._norandom:
+                    encouraged: List[Tuple[history.Action, float]]\
+                            = actions[:1]
+                else:
+                    encouraged: List[Tuple[history.Action, float]]\
+                            = [ ( self._random_action(key, True)
+                                , self._rng.random()/2.
+                                )
+                              ]
+            else:
+                encouraged: List[Tuple[history.Action, float]] = actions[:1]
+            encouraged_actions: Set[history.Action] = set(map(lambda itm: itm[0], encouraged))
+            logger.info(encouraged)
+            logger.info(encouraged_actions)
+            encouraged: str = "\n".join( map( lambda act: self._action_to_string(act[0], act[1])
+                                            , encouraged
+                                            )
+                                       )
+            if actions[-1][1]>0.:
+                if self._norandom:
+                    discouraged: List[Tuple[history.Action, float]]\
+                            = actions[-1:]
+                else:
+                    discouraged_action: history.Action = self._random_action(key, False)
+                    j = 0
+                    while discouraged_action in encouraged_actions:
+                        discouraged_action = self._random_action(key, False)
+                        j += 1
+                        if j>=10:
+                            break
+                    discouraged: List[Tuple[history.Action, float]]\
+                            = [ ( discouraged_action
+                                , 0.
+                                )
+                              ]
+                logger.debug("Generated Discouraged: {:}".format(discouraged))
+            else:
+                discouraged: List[Tuple[history.Action, float]] = list( itertools.takewhile( lambda itm: itm[1]==0.
+                                                                      , reversed(actions)
+                                                                      )
+                                                           )
+                logger.debug("Recorded Discouraged: {:}".format(discouraged))
+            discouraged: str = "\n".join( map( lambda act: self._action_to_string(act[0], act[1])
+                                             , discouraged
+                                             )
+                                        )
+
+            examplar: str = self._examplar_to_string( i
+                                                    , key
+                                                    , info_dict
+                                                    , encouraged
+                                                    , discouraged
+                                                    )
+            #  }}} Contruct one Examplar # 
+
+            examplar_length: int = len(self._tokenizer.encode(examplar,bos=True,eos=True))+1
+            if examplar_length<=example_tokens_limit:
+                examplars.append(examplar)
+                examplar_ids.append(record["id"])
+                examplar_scores.append(score)
+                example_tokens_limit -= examplar_length
+                i += 1
+                if i>=nb_examplars:
+                    break
+        #  }}} Construct Examplars # 
+
+        logger.debug("Egs: %s", " ".join(map(str, examplar_ids)))
+        logger.debug("Sms: %s", " ".join(map("{:.2f}".format, examplar_scores)))
+        assert len(examplar_ids)>=1
+
+        return examplars
+        #  }}} method _get_examplars # 
+
+    @abc.abstractmethod
+    def _random_action(self, key: history.Key, encourages: bool = False) -> history.Action:
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def _action_to_string(self, action: history.Action, value: float) -> str:
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def _examplar_to_string( self
+                           , index: int
+                           , key: history.Key
+                           , info_dict: history.HistoryReplay.InfoDict[history.Action]
+                           , encouraged: str
+                           , discouraged: str
+                           ) -> str:
+        raise NotImplementedError()
+
+    def train(self, train: bool):
+        self._train = train
+    #  }}} class HistoryReplayClient # 
+class LlamaHistoryReplayClient(Generic[history.Key, history.Action]):
     def __init__( self
                 , history_replay: history.HistoryReplay[history.Key, history.Action]
                 , train: bool
